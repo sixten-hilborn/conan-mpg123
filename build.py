27da5968aeb77b348c2de235992144b4a3a60d73
Sixten Hilborn <you@example.com>
2018-04-10 23:14:56 +0200

    Avoid windows static builds in CI

--- build.py
+++ build.py
@@ -3,9 +3,11 @@
 
 
 from bincrafters import build_template_default
+import platform
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-
+    if platform.system() == "Windows":
+        builder.items = [build for build in builder.items if build[1]["shared"]]
     builder.run()
