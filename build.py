12dde098966b24b080810ee716bde734898b44b9
Sixten Hilborn <you@example.com>
2018-04-10 23:18:57 +0200

    Fix windows ci

--- build.py
+++ build.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
     if platform.system() == "Windows":
-        builder.items = [build for build in builder.items if build[1]["shared"]]
+        builder.items = [build for build in builder.items if build[1]["mpg123:shared"]]
     builder.run()
