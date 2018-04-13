06a2ee884d6f5428328fb6d85aeca355098bbcd5
Sixten Hilborn <you@example.com>
2018-04-13 23:25:30 +0200

    [CI] Build shared+MT

--- build.py
+++ build.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default
-import platform
+from bincrafters import build_shared
 
 if __name__ == "__main__":
-
-    builder = build_template_default.get_builder()
+    builder = build_shared.get_builder()
+    builder.add_common_builds(shared_option_name="mpg123:shared", pure_c=True, dll_with_static_runtime=True)
 
     builder.run()
